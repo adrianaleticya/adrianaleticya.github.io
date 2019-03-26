3c784c88da04d6fe448dbdd972a5074c3e5e55de
Osvaldo Aurélio <osvaldo.aurelio@outlook.com>
2019-03-26 00:46:50 -0300

    creating gitignore
    fixing close drop menu at mobile viewport

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
-document.querySelectorAll('div.navbar-nav > a.nav-item').forEach(
-    el => el.addEventListener("click", 
-        self => {
-            document.querySelectorAll('main > section').forEach(
-                elem => elem.style.display = 'none'
-            )
-            document.getElementById(self.target.href.split('#')[1]).style.display = 'block'
-        }
-    )
-)
-window.addEventListener('resize', () => setCarouselImages())
 window.addEventListener('DOMContentLoaded', () => setCarouselImages())
-
+window.addEventListener('resize', () => setCarouselImages())
+const $ = document.getElementById.bind(document)
+const imgCarouselPath = './assets/img/carouselSlide'
+document.querySelectorAll('div.navbar-nav > a.nav-item').forEach(
+    el => el.addEventListener("click", self => {
+        document.querySelectorAll('main > section').forEach(el => el.style.display = 'none')
+        $(self.target.href.split('#')[1]).style.display = 'block'
+        document.querySelector('.navbar-collapse').classList.remove('show')
+    })
+)
 let setCarouselImages = () => {
-    let carouselImg = [
-        document.getElementById('carousel-img-0'), 
-        document.getElementById('carousel-img-1'), 
-        document.getElementById('carousel-img-2')
-    ]
+    let carouselImg = [$('carousel-img-0'), $('carousel-img-1'), $('carousel-img-2')]
     for(let i = 0; i < 3; i++) 
         carouselImg[i].setAttribute(
-            'src', `./assets/img/carouselSlide/${screen.width < 800 ? 'mobile' : 'desktop'}/${i}.jpg`
+            'src', `${imgCarouselPath}/${screen.width<screen.height?'mobile':'desktop'}/${i}.jpg`
         )
 }
