e72317e3d9ac204acebab3aca576812dd6f472cd
Osvaldo Aurélio <osvaldo.aurelio@outlook.com>
2019-03-31 12:50:47 -0300

    Adding Video-slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => setCarouselImages())
-window.addEventListener('resize', () => setCarouselImages())
+// window.addEventListener('DOMContentLoaded', () => setCarouselImages())
+// window.addEventListener('resize', () => setCarouselImages())
 const $ = document.getElementById.bind(document)
 const imgCarouselPath = './assets/img/carouselSlide'
 document.querySelectorAll('div.navbar-nav > a.nav-item').forEach(
     el => el.addEventListener("click", self => {
-        document.querySelectorAll('main > section').forEach(el => el.style.display = 'none')
-        $(self.target.href.split('#')[1]).style.display = 'block'
+        // document.querySelectorAll('main > section').forEach(el => el.style.display = 'none')
+        // $(self.target.href.split('#')[1]).style.display = 'block'
         document.querySelector('.navbar-collapse').classList.remove('show')
     })
 )
-let setCarouselImages = () => {
-    let carouselImg = [$('carousel-img-0'), $('carousel-img-1'), $('carousel-img-2')]
-    for(let i = 0; i < 3; i++) 
-        carouselImg[i].setAttribute(
-            'src', `${imgCarouselPath}/${screen.width<screen.height?'mobile':'desktop'}/${i}.jpg`
-        )
-}
+// let setCarouselImages = () => {
+//     let carouselImg = [$('carousel-img-0'), $('carousel-img-1'), $('carousel-img-2')]
+//     for(let i = 0; i < 3; i++) 
+//         carouselImg[i].setAttribute(
+//             'src', `${imgCarouselPath}/${screen.width<screen.height?'mobile':'desktop'}/${i}.jpg`
+//         )
+// }
